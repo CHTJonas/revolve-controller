be92528628107ba56d1e14dbca60dbb5454c40b1
Wai-Wai Ng <me@waiwaing.com>
2016-11-10 03:21:15 +0000

    David did more things

---
+++ state_machine.c
@@ -0,0 +1,7 @@
+// 
+// 
+// 
+
+#include "state_machine.h"
+
+
---
+++ state_machine.h
@@ -0,0 +1,31 @@
+#pragma once
+
+enum StateEnum {
+	STATE_MAINMENU,
+	STATE_RUN,
+};
+
+enum RunStateEnum {
+	STATE_RUN_READY,
+	STATE_RUN_DRIVE,
+	STATE_RUN_BRAKE,
+	STATE_RUN_ESTOP
+};
+
+typedef struct State {
+	enum StateEnum state;
+
+	union {
+		struct mainmenu {};
+		struct {
+			enum RunStateEnum state;
+
+			union {
+				struct ready {};
+				struct drive {};
+				struct brake {};
+				struct estop {};
+			} data;
+		} run;
+	} data;
+} State;
