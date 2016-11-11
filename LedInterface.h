30142a6e59e385c04f528911b9cc3fcd35bce8ef
Wai-Wai Ng <me@waiwaing.com>
2016-11-11 23:41:59 +0000

    Move LEDs into separate class

---
+++ LedInterface.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "cuestack.h"
+#include <Adafruit_NeoPixel.h>
+#include <Bounce2.h>
+#include <Encoder.h>
+#include <Keypad.h>
+
+class LedInterface
+{
+public:
+	LedInterface(Adafruit_NeoPixel& ringLeds, Adafruit_NeoPixel& pauseLeds, Adafruit_NeoPixel& keypadLeds);
+
+	void setupLeds();
+	void limitLedSettings();
+
+	void flashLed(int led, int interval);
+	void updatePauseLeds() const;
+	void ringLedsColor(int r, int g, int b) const;
+	void pauseLedsColor(int r, int g, int b) const;
+	void keypadLedsColor(int r, int g, int b) const;
+	static void encRed();
+	static void encGreen();
+	static void encBlue();
+	static void encOff();
+	void allLedsOn() const;
+	void allLedsOff() const;
+
+	Adafruit_NeoPixel& ringLeds;
+	Adafruit_NeoPixel& pauseLeds;
+	Adafruit_NeoPixel& keypadLeds;
+
+	int ledSettings[4];
+
+private:
+	long int flashCounter = 0;
+
+};
+
