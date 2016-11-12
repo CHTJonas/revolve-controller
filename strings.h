7679e871dcffb3950dc3eae6f2fc6fe7609199a0
Charlie Jonas <charlie@charliejonas.co.uk>
2016-11-12 03:48:15 +0000

    Logo & strings split into separate file.

---
+++ strings.h
@@ -0,0 +1,54 @@
+#pragma once
+
+// startup strings
+#define TITLE "Panto Revolve"
+#define SUBTITLE "Controller"
+#define NAME "Designed by Jack"
+#define DATE "August 2016"
+
+// main menu strings
+#define MENU_OPTION_1 "Manual Operation"
+#define MENU_OPTION_2 "Program Cues"
+#define MENU_OPTION_3 "Show Mode"
+#define MENU_OPTION_4 "Settings"
+
+// settings menu strings
+#define SETTINGS_OPTION_1 "Home Revolve"
+#define SETTINGS_OPTION_2 "PID Constants"
+#define SETTINGS_OPTION_3 "Default Cue Values"
+#define SETTINGS_OPTION_4 "Backup Cuestack"
+#define SETTINGS_OPTION_5 "Reset Cuestack"
+#define SETTINGS_OPTION_6 "Encoder Settings"
+#define SETTINGS_OPTION_7 "LED Settings"
+#define SETTINGS_OPTION_8 "Hardware Test"
+
+// encoder menu strings
+#define ENCODER_OPTION_1 "Inner Dir:"
+#define ENCODER_OPTION_2 "Outer Dir:"
+#define ENCODER_OPTION_3 "Inner RO:"
+#define ENCODER_OPTION_4 "Outer RO:"
+
+// LED menu strings
+#define LED_OPTION_1 "Brightness:"
+#define LED_OPTION_2 "LEDs Red:"
+#define LED_OPTION_3 "LEDs Green:"
+#define LED_OPTION_4 "LEDs Blue"
+
+// PID constants menu strings
+#define PID_OPTION_1 "Inner kp_0:"
+#define PID_OPTION_2 "Inner kp_smin:"
+#define PID_OPTION_3 "Inner kp_amax:"
+#define PID_OPTION_4 "Outer kp_0:"
+#define PID_OPTION_5 "Outer kp_smin:"
+#define PID_OPTION_6 "Outer kp_amax:"
+
+// programming menu strings
+#define PROGRAM_OPTION_1 "Next Cue"
+#define PROGRAM_OPTION_2 "Previous Cue"
+#define PROGRAM_OPTION_3 "Add Cue"
+#define PROGRAM_OPTION_4 "Delete Cue"
+
+// parameter menu strings
+#define PARAM_OPTION_1 "Auto Follow:"
+#define PARAM_OPTION_2 "Inner EN:"
+#define PARAM_OPTION_3 "Outer EN:"
