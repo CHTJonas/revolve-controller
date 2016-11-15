489f6c7d08797c639fcd2f5678286eda0e42f131
Charlie Jonas <charlie@charliejonas.co.uk>
2016-11-15 22:34:33 +0000

    Fix for menus larger than 4 items

--- menu.cpp
+++ menu.cpp
@@ -15,20 +15,6 @@ Menu::~Menu() {
 	delete[] m_menuOptionStrings;
 }
 
-void Menu::draw() {
-    m_screen.setFont(large_font);
-    for (int i = 0; i < m_numberOfOptions; i++) {
-        if (i == m_interface.menu_pos) {
-            // Position highlight box from top left corner
-            m_screen.drawBox(0, (m_interface.menu_pos * 16), SCREEN_WIDTH, 16);
-            m_screen.setDefaultBackgroundColor();
-            drawStrCentre((i * 16) + 12, m_menuOptionStrings[i]);
-        }
-        drawStrCentre((i * 16) + 12, m_menuOptionStrings[i]);
-        m_screen.setDefaultForegroundColor();
-    }
-}
-
 void Menu::drawStrCentre(int y, const char* text) {
 	int width = m_screen.getStrWidth(text);
 	int x = (SCREEN_WIDTH - width) / 2;
@@ -39,4 +25,20 @@ void Menu::drawStrCentre(int y, const char* text) {
 void Menu::drawStrCentre(int y, char text) {
 	const char string[] = { text, '\0' };
 	drawStrCentre(y, string);
+}
+
+void Menu::draw() {
+    m_screen.setFont(large_font);
+    // we can display max 4 options on the screen at a time
+    int max = m_numberOfOptions < 4 ? m_numberOfOptions : 4;
+    for (int i = 0; i < max; i++) {
+        if ((i + m_numberOfOptions / 4) == m_interface.menu_pos) {
+            // Position highlight box from top left corner
+            m_screen.drawBox(0, (m_interface.menu_pos * 16), SCREEN_WIDTH, 16);
+            m_screen.setDefaultBackgroundColor();
+            drawStrCentre((i * 16) + 12, m_menuOptionStrings[i + m_numberOfOptions / 4]);
+        }
+        drawStrCentre((i * 16) + 12, m_menuOptionStrings[i + m_numberOfOptions / 4]);
+        m_screen.setDefaultForegroundColor();
+    }
 }
