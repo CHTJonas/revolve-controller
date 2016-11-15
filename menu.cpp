8e8f7723f872fbafed34b3b507ba01332ec8a902
Charlie Jonas <charlie@charliejonas.co.uk>
2016-11-15 15:55:05 +0000

    Begin work on menu class

--- menu.cpp
+++ menu.cpp
@@ -1,4 +1,13 @@
 #include "menu.h"
 
-Menu::Menu(int size) {
+Menu::Menu(const char* menuOptionStrings[], int numberOfOptions) {
+    m_menuOptionStrings = new const char*[numberOfOptions];
+
+    for (int i = 0; i < numberOfOptions; i++) {
+      m_menuOptionStrings[i] = menuOptionStrings[i];
+   }
+}
+
+Menu::~Menu() {
+    delete[] m_menuOptionStrings;
 }
